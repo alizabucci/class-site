51f260277f806dbad3e8db1a79aa756bd18db961
alizabucci <69876564+alizabucci@users.noreply.github.com>
2020-10-21 12:08:10 -0400

    Aura marquee delay start updated

--- Aura Project_visual feeling/functions.js
+++ Aura Project_visual feeling/functions.js
@@ -5,7 +5,7 @@ $('.marquee').marquee({
 	//gap in pixels between the tickers
 	gap: 50,
 	//time in milliseconds before the marquee will start animating
-	delayBeforeStart: 2000,
+	delayBeforeStart: 5000,
 	//'left' or 'right'
 	direction: 'right',
 	//true or false - should the marquee be duplicated to show an effect of continues flow
